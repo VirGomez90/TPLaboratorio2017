695656b63010d8ba6dca2aac5469c89c445ebe99
Rocio Altamirano <rochyaltamirano@gmail.com>
2017-10-08 22:40:07 -0300

    Actualización de clase Conexión

--- Salome/src/persistencia/conexion/Conexion.java
+++ Salome/src/persistencia/conexion/Conexion.java
@@ -3,86 +3,41 @@ package persistencia.conexion;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-import dto.ConfiguracionDTO;
-import persistencia.dao.ConfiguracionDAO;
-
 public class Conexion {
 
 
 	public static Conexion instancia;
-	private Connection conexion;
-	private boolean open;
-	
-	
-	
-	/**
-	 * ctor
-	 * @param
-	 * */
-	public Conexion() {
-				
-		try {
-			
-			ConfiguracionDAO configdelsystema = new ConfiguracionDAO();
-			ConfiguracionDTO config = configdelsystema.obtenerConfiguracion("config.ini");
-						
-			conexion = DriverManager.getConnection("jdbc:mysql://" + config.getIp() + ":" + config.getPuerto() +"/"+config.getNombrebasedatos(), config.getUsuario(), config.getPassword());	
-			System.out.println("Conexion exitosa");
-			setOpen(true);
-	
-		} catch (Exception e) {
-			System.out.println("Conexion fallida");
-			setOpen(false);
-			cerrarConexion();
+		private Connection conexion;
+		
+		public Conexion()
+		{
+			try
+			{
+				conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/agenda","root","root");
+				System.out.println("Conexion exitosa");
+			}
+			catch(Exception e)
+			{
+				System.out.println("Conexion fallida");
+			}
 		}
-	}
-
-	
-	
-	/**
-	 * @param
-	 * */
-	public static Conexion getConexion() {
-		if (instancia == null) {
-			instancia = new Conexion();
+		
+		public static Conexion getConexion()   
+		{								
+			if(instancia == null)
+			{
+				instancia = new Conexion();
+			}
+			return instancia;
 		}
-		return instancia;
-	}
 
-	
-	
-	/**
-	 * @param
-	 * */
-	public Connection getSQLConexion() {
-		return conexion;
-	}
-
-	
-	
-	/**
-	 * @param
-	 * */
-	public void cerrarConexion() {
-		instancia = null;
-	}
-
-	
-	
-	/**
-	 * @return the open
-	 */
-	public boolean isOpen() {
-		return open;
-	}
-
-	
-	
-	/**
-	 * @param open the open to set
-	 */
-	public void setOpen(boolean open) {
-		this.open = open;
-	}
+		public Connection getSQLConexion() 
+		{
+			return conexion;
+		}
+		
+		public void cerrarConexion()
+		{
+			instancia = null;
+		}
 }
-	
